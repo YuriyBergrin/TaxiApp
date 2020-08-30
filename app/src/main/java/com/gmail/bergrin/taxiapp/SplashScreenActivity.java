7ccbe8c94e7c6f7ab700c650aa8ef18009361812
YuriyBergrin <bergrin1991@gmail.com>
2020-08-30 14:20:10 +0300

    Initial commit

---
+++ app/src/main/java/com/gmail/bergrin/taxiapp/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.gmail.bergrin.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreenActivity.this, ChooseModeActivity.class));
+                }
+            }
+        };
+
+        thread.start();
+
+
+    }
+}
